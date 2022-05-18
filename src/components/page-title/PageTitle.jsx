76d3cdf6c5b90ce65736a88457300fdbecc25a47
Daniel <danielnatsufire@gmail.com>
2022-05-17 23:20:51 -0300

    Sucesso

--- src/components/page-title/PageTitle.jsx
+++ src/components/page-title/PageTitle.jsx
@@ -2,7 +2,7 @@ import './PageTitle.css'
 
 export default function PageTitle({ title, color }) {
     return (
-        <div className="page-title" style={{ color }}>
+        <div className="page-title" style={{ color, fontWeight: color ? 'bold' : 'normal' }}>
             <h1>{title}</h1>
         </div>
     )
--- src/components/success/Success.jsx
+++ src/components/success/Success.jsx
@@ -1,9 +1,32 @@
 import './Success.css'
+import PageTitle from './../page-title/PageTitle';
+import { Link } from 'react-router-dom';
 
 export default function Success({ order, setOrder }) {
-    console.log(order)
+    const cpf = order.cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4")
 
     return (
-        <div className="success">sucesso</div>
+        <div className="success">
+            <PageTitle title="Pedido feito com sucesso!" color="#247A6B" />
+            <div>
+                <div className="info">
+                    <strong>Filme e sessão</strong>
+                    <p>{order.title}</p>
+                    <p>{order.date} {order.weekday}</p>
+                </div>
+                <div className="info">
+                    <strong>Ingressos</strong>
+                    {order.ids.map(id => <p>Assento {id}</p>)}
+                </div>
+                <div className="info">
+                    <strong>Comprador</strong>
+                    <p>Nome: {order.name}</p>
+                    <p>CPF: {cpf}</p>
+                </div>
+            </div>
+            <Link to="/">
+                <button className="home">Voltar pra Home</button>
+            </Link>
+        </div>
     )
 }
