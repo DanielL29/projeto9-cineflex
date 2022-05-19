70b0571837d1fac251331ccb2dc7e8fa3dca0870
Daniel <danielnatsufire@gmail.com>
2022-05-18 22:03:50 -0300

    Bonus - Informação de comprador por assento(não exatamente o esperado)

--- src/components/seats/Seats.jsx
+++ src/components/seats/Seats.jsx
@@ -5,18 +5,87 @@ import PageTitle from '../page-title/PageTitle'
 import './Seats.css'
 import Footer from '../footer/Footer';
 
-function Seat({ number, status, id, ids, setIds }) {
+function Seat({ number, status, id, ids, setIds, buyer, setBuyer }) {
     const [color, setColor] = useState(false)
     const selectSeat = color ? 'green' : ''
 
     function getSeatId(id) {
-        setColor(!color)
-        setIds([...ids, id])
+        if (color) {
+            if (window.confirm('Deseja remover o assento e apagar os dados?')) {
+                for (let i = 0; i < ids.length; i++) {
+                    if (id === ids[i]) {
+                        ids.splice(i, 1)
+                    }
+                    if (buyer.length > 0) {
+                        for (let j = 0; j < buyer.length; j++) {
+                            if (id === buyer[j].id) {
+                                buyer.splice(j, 1)
+                            }
+                        }
+                    }
+                }
+                setColor(!color)
+                setIds([...ids])
+                setBuyer([])
+            }
+        } else {
+            setColor(!color)
+            setIds([...ids, id])
+        }
     }
 
     return (
-        <div className={`seat ${!status ? 'yellow' : selectSeat}`} 
+        <div className={`seat ${!status ? 'yellow' : selectSeat}`}
             onClick={status ? () => getSeatId(id) : () => alert('Esse assento não está disponível')}>{number}</div>
+    )
+}
+
+function UserFields({ id, seats, buyer, setBuyer, validate }) {
+    const [name, setName] = useState('')
+    const [cpf, setCPF] = useState('')
+    const cpfValid = cpf.length === 11
+    const noBuyer = buyer.length === 0
+    const seatNumber = seats.find(seat => seat.id === id)
+
+    useEffect(() => {
+        if (cpf !== "") {
+            getBuyer()
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [cpfValid])
+
+    useEffect(() => {
+        if (buyer.length === 0) {
+            setName('')
+            setCPF('')
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [noBuyer])
+
+    function getBuyer() {
+        setBuyer([...buyer, { id, name, cpf }])
+    }
+
+    function limitCPF(e) {
+        if (e.target.value.length <= 11) {
+            setCPF(e.target.value)
+        }
+    }
+
+    return (
+        <div>
+            <h1>Comprador do assento: {seatNumber.name}</h1>
+            <div>
+                <label>Nome do comprador:</label>
+                <input type="text" placeholder="Digite seu nome..." value={name} onChange={(e) => setName(e.target.value)} />
+                {name === "" && validate ? <p>Nome não informado</p> : ''}
+            </div>
+            <div>
+                <label>CPF do comprador:</label>
+                <input type="text" placeholder="Digite seu CPF..." value={cpf} onChange={(e) => limitCPF(e)} />
+                {cpf.length < 11 && validate ? <p>O campo de CPF precisa ter 11 caracteres</p> : ''}
+            </div>
+        </div>
     )
 }
 
@@ -25,9 +94,9 @@ export default function Seats({ order, setOrder, setPreviousPath }) {
     const [seats, setSeats] = useState([])
     const [day, setDay] = useState({})
     const [film, setFilm] = useState({})
-    const [name, setName] = useState('')
-    const [cpf, setCPF] = useState('')
     const [ids, setIds] = useState([])
+    const [buyer, setBuyer] = useState([])
+    const [validate, setValidate] = useState(false)
 
     useEffect(() => {
         getSeats()
@@ -43,29 +112,31 @@ export default function Seats({ order, setOrder, setPreviousPath }) {
     }
 
     function sendOrder() {
-        let orderData = { ids, name, cpf }
         let counter = 0
-        ids.sort((a, b) => a - b)
-        const getSeatsName = (seat) => {
-            if(seat.id === ids[counter]) {
-                counter++
-                return seat.name
+        setValidate(true)
+        if (ids.length > 0 && ids.length === buyer.length) {
+            ids.sort((a, b) => a - b)
+            const getSeatsName = (seat) => {
+                if (seat.id === ids[counter]) {
+                    counter++
+                    return seat.name
+                }
             }
+            const idsName = seats.map(getSeatsName).filter(name => name !== undefined).map((user, i) => {
+                return { idAssento: user, nome: buyer[i].name, cpf: buyer[i].cpf }
+            })
+            let orderData = { ids, compradores: idsName }
+            setOrder({
+                ...order,
+                title: film.title,
+                weekday: day.weekday,
+                date: day.date,
+                ids: idsName,
+            })
+
+            const promise = axios.post('https://mock-api.driven.com.br/api/v5/cineflex/seats/book-many', orderData)
+            promise.catch(() => alert('Solicitação não enviada!'))
         }
-        const idsName = seats.map(getSeatsName).filter(name => name !== undefined)
-        setOrder({
-            ...order,
-            title: film.title,
-            weekday: day.weekday,
-            date: day.date,
-            ids: idsName,
-            name,
-            cpf
-        })
-        
-        const promise = axios.post('https://mock-api.driven.com.br/api/v5/cineflex/seats/book-many', orderData)
-        promise.then(res => console.log(res.data))
-        promise.catch(res => res.response.data)
     }
 
     return (
@@ -74,12 +145,14 @@ export default function Seats({ order, setOrder, setPreviousPath }) {
             <div className="seats">
                 {seats.map(seat => {
                     return (
-                        <Seat key={seat.id} 
-                            status={seat.isAvailable} 
-                            number={seat.name} 
-                            ids={ids} 
+                        <Seat key={seat.id}
+                            status={seat.isAvailable}
+                            number={seat.name}
+                            ids={ids}
                             setIds={setIds}
                             id={seat.id}
+                            buyer={buyer}
+                            setBuyer={setBuyer}
                         />
                     )
                 })}
@@ -94,19 +167,22 @@ export default function Seats({ order, setOrder, setPreviousPath }) {
                     <p>Disponível</p>
                 </div>
                 <div>
-                    <div className="seat yellow"></div> 
+                    <div className="seat yellow"></div>
                     <p>Indisponível</p>
                 </div>
             </div>
             <div className="user-form">
-                <div>
-                    <label>Nome do comprador:</label>
-                    <input type="text" placeholder="Digite seu nome..." value={name} onChange={(e) => setName(e.target.value)} />
-                </div>
-                <div>
-                    <label>CPF do comprador:</label>
-                    <input type="text" placeholder="Digite seu CPF..." value={cpf} onChange={(e) => setCPF(e.target.value)} />
-                </div>
+                {ids.sort((a, b) => a - b).map((id, i) => {
+                    return (
+                        <UserFields key={i}
+                            id={id}
+                            seats={seats}
+                            buyer={buyer}
+                            setBuyer={setBuyer}
+                            validate={validate}
+                        />
+                    )
+                })}
             </div>
             <Link to="/sucesso" onClick={() => setPreviousPath(`/assentos/${id}`)}>
                 <button className="send" onClick={sendOrder}>Reservar assento(s)</button>
--- src/components/success/Success.jsx
+++ src/components/success/Success.jsx
@@ -3,8 +3,6 @@ import PageTitle from './../page-title/PageTitle';
 import { Link } from 'react-router-dom';
 
 export default function Success({ order, setPreviousPath }) {
-    const cpf = order.cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4")
-
     return (
         <div className="success">
             <PageTitle title="Pedido feito com sucesso!" color="#247A6B" />
@@ -16,12 +14,19 @@ export default function Success({ order, setPreviousPath }) {
                 </div>
                 <div className="info">
                     <strong>Ingressos</strong>
-                    {order.ids.map(id => <p>Assento {id}</p>)}
+                    {order.ids.map(id => <p key={id.idAssento}>Assento {id.idAssento}</p>)}
                 </div>
                 <div className="info">
-                    <strong>Comprador</strong>
-                    <p>Nome: {order.name}</p>
-                    <p>CPF: {cpf}</p>
+                    {order.ids.map(id => {
+                        let cpf = id.cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4")
+                        return (
+                            <div key={id.idAssento}>
+                                <strong>Comprador do Assento {id.idAssento}</strong>
+                                <p>Nome: {id.nome}</p>
+                                <p>CPF: {cpf}</p>
+                            </div>
+                        )
+                    })}
                 </div>
             </div>
             <Link to="/" onClick={() => setPreviousPath("/")}>
